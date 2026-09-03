e8c015e8c9edb6ea814122d3eb184cb79d655c07
chikoci <10241017@student.itk.ac.id>
2026-09-03 13:59:03 +0800

    penambahan sesuai modul bagian build

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ('Selamat datan');
+    return view('welcome');
+});
+
+Route::get('/tentang', function () {
+    return view('tentang');
 });
